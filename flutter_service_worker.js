c1854b9662a6382a7055f4e2ded101b83d519b7a
niho2 <115228830+niho2@users.noreply.github.com>
2023-08-27 12:24:30 +0200

    Parkage
    Name wurde geändert

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "a53491ba33870e68dce7c820b0867cda
 "icons/Icon-512.png": "8cba04fe10ca55138014a365dad48a8b",
 "icons/Icon-maskable-192.png": "88c6eec8c79f9bdbb495e6b0b5b746fa",
 "icons/Icon-maskable-512.png": "8cba04fe10ca55138014a365dad48a8b",
-"index.html": "23f125eea9e8afc8c4faae38fcacbb20",
-"/": "23f125eea9e8afc8c4faae38fcacbb20",
+"index.html": "e218201831c8865a4b17a21ac5713583",
+"/": "e218201831c8865a4b17a21ac5713583",
 "main.dart.js": "12ebb036d71192834135b018500cf12e",
 "manifest.json": "653606e67c4d915a66cfd2c97ef4e2dc",
 "version.json": "2c8cc99f4198ac27ae4a6e04e1dbae90"};
